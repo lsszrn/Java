fed17fe50e68a650f887b6781734960565ecf62a
lsszrn <alyaska2201@mail.ru>
2026-04-17 18:18:50 +0300

    Create A.java

---
+++ A.java
@@ -0,0 +1,23 @@
+/**
+ * Класс с перегруженными методами printNum.
+ *
+ * @author lsszrn
+ * @version 1.0
+ */
+public class A {
+    public void printNum(Integer i) {
+        System.out.printf("Integer = %d%n", i);
+    }
+    
+    public void printNum(int i) {
+        System.out.printf("int = %d%n", i);
+    }
+    
+    public void printNum(Float f) {
+        System.out.printf("Float = %.4f%n", f);
+    }
+    
+    public void printNum(Number n) {
+        System.out.println("Number = " + n);
+    }
+}
