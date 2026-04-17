7a9b72a343512f03086f841ea031e0ae25493de7
lsszrn <alyaska2201@mail.ru>
2026-04-17 18:15:01 +0300

    Create A.java

---
+++ A.java
@@ -0,0 +1,15 @@
+/**
+ * Класс со статическими полями и методом.
+ *
+ * @author lsszrn
+ * @version 1.0
+ */
+public class A {
+    public static int a = 1;
+    public static int b = 2;
+    
+    public static void printVars() {
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
